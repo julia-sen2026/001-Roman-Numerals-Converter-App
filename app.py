4fcf2734992528b18387eef96703ae38a36d5c68
Julia Sen <julia.sen2026@gmail.com>
2026-06-15 15:11:15 +0200

    initial commit: Roman numerals app structure

---
+++ app.py
@@ -0,0 +1,4 @@
+#As a first step of the project, you need to write program that converts the given number (between 1 and 3999) to the roman numerals. 
+# The program should convert only from numbers to Roman numerals, 
+# not vice versa and during the conversion following notes should be taken into consideration.
+
